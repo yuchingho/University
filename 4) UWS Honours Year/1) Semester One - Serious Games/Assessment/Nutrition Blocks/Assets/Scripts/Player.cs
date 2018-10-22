e41cd626e5edc7e910850b508050073480509638
Yu-Ching <yuchingho111@gmail.com>
2018-10-22 22:51:46 +0100

    Update

--- 4) UWS Honours Year/1) Semester One - Serious Games/Assessment/Nutrition Blocks/Assets/Scripts/Player.cs
+++ 4) UWS Honours Year/1) Semester One - Serious Games/Assessment/Nutrition Blocks/Assets/Scripts/Player.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    Rigidbody2D Rigidbody2D;
+    BoxCollider2D BoxCollider2D;
+    Animator Animator;
+    public float Speed;
+
+	void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        BoxCollider2D = GetComponent<BoxCollider2D>();
+        Animator = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {   // To not flip character, Rigidbody2D Contraints freeze Z
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Animator.Play("Running");
+            transform.localScale = new Vector2(-1, 1);  // x == left/right | y == up/down
+            transform.position += Vector3.left * Time.deltaTime * Speed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Animator.Play("Idle");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Animator.Play("Running");
+            transform.localScale = new Vector2(1, 1);
+            transform.position += Vector3.right * Time.deltaTime * Speed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Animator.Play("Idle");
+        }
+    }
 }
