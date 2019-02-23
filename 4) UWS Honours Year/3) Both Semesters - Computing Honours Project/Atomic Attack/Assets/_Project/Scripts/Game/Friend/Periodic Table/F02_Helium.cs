a4b42340f98dcac83385cc5fb56c8722ab89487c
Yu-Ching <yuchingho111@gmail.com>
2019-02-23 17:13:57 +0000

    Update

--- 4) UWS Honours Year/3) Both Semesters - Computing Honours Project/Atomic Attack/Assets/_Project/Scripts/Game/Friend/Periodic Table/F02_Helium.cs
+++ 4) UWS Honours Year/3) Both Semesters - Computing Honours Project/Atomic Attack/Assets/_Project/Scripts/Game/Friend/Periodic Table/F02_Helium.cs
@@ -9,45 +9,47 @@ public class F02_Helium : MonoBehaviour {
 
     public int CostValue;
     public int ScoreValue;
-    [SerializeField] protected float MovementSpeed = 1f;
+    [SerializeField] protected int MovementSpeed = 1;
     [SerializeField] GameObject Explosion;
     [SerializeField] GameObject SpawnUnits;
+    bool Spawn;
 
     void Start()
     {
         HealthSystem = GetComponent<HealthSystem>();
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Spawn = false;
     }
 
     void Update()
     {
         transform.Translate(Vector2.right * MovementSpeed * Time.deltaTime);
-        if (HealthSystem.Deceased == true || transform.position.x >= 5.75)
+        if (transform.position.x >= 5.5 || HealthSystem.Deceased == true)
         {
-            Rigidbody2D.gravityScale = 1;
-            if (transform.position.y <= -1.45f)
-            {
-                Destroy(gameObject);
-                Instantiate(Explosion, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
-            }
-            // damage building 
-            // and units 
-            // and explosion blowback
+            if (Spawn == false) { SpawnFlying(); }
             if (HealthSystem.Deceased == true)
-            {   // Only if E_Gunmen kill the Blimp, add Score.
-                // score
+            {
+                Rigidbody2D.gravityScale = 1;
+                if (transform.position.y <= -1.45f)
+                {
+                    // damage building 
+                    // and units 
+                    // and explosion blowback
+                    Destroy(gameObject);
+                    Instantiate(Explosion, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+                }
             }
-            SpawnFlying();
         }
+        if (transform.position.x >= 14) { Destroy(gameObject); }
     }
 
     void SpawnFlying()
     {
-        //Instantiate(ShockUnits, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
-        // Spawn 5, and when drops down, can see the 5 in a row.
+        Instantiate(SpawnUnits, new Vector2(transform.position.x + 0.4f, transform.position.y), Quaternion.identity);
+        Instantiate(SpawnUnits, new Vector2(transform.position.x + 0.2f, transform.position.y), Quaternion.identity);
+        Instantiate(SpawnUnits, new Vector2(transform.position.x - 0.0f, transform.position.y), Quaternion.identity);
+        Instantiate(SpawnUnits, new Vector2(transform.position.x - 0.2f, transform.position.y), Quaternion.identity);
+        Instantiate(SpawnUnits, new Vector2(transform.position.x - 0.4f, transform.position.y), Quaternion.identity);
+        Spawn = true;
     }
-
-    // instiate blimp at spawn location + y.
-    // obj will float along top of screen, will have HP
-    // once destroyed, spawn swordsman which will drop down and start running along ground
 }
---
+++ 4) UWS Honours Year/3) Both Semesters - Computing Honours Project/Atomic Attack/Assets/_Project/Scripts/Game/ManagerSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSpawn : MonoBehaviour {
+
+	void Start()
+    {
+		
+	}
+	
+	void Update() 
+    {
+		
+	}
+}
